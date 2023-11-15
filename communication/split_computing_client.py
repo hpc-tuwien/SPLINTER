45e1f30b1c9359f6ecf5706ab5e6b072dce2de58
Daniel May <e11809922@student.tuwien.ac.at>
2023-11-15 18:14:37 +0100

    Add TPU usage

--- communication/split_computing_client.py
+++ communication/split_computing_client.py
@@ -1,3 +1,5 @@
+import argparse
+import os
 import time
 
 import grpc
@@ -5,6 +7,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import tflite_runtime.interpreter as tflite
 from keras.applications import mobilenet_v2
 from keras.applications import resnet50
 from keras.applications import vgg16
@@ -53,8 +56,9 @@ class SplitComputeClient:
         self.input_zero_point = None
         self.output_scale = None
         self.output_zero_point = None
+        self.accelerator = None
 
-    def split_compute(self, stub, image, network, partition_index):
+    def split_compute(self, stub, image, network, partition_index, accelerator):
         start_counter_ns = time.perf_counter_ns()
 
         # check if there is any local computation
@@ -68,15 +72,24 @@ class SplitComputeClient:
             self.input_zero_point = None
             self.output_scale = None
             self.output_zero_point = None
+            self.accelerator = accelerator
             intermediate = image
         else:
-            if (network == self.network) and (partition_index == self.partition_index):
+            if (network == self.network) and (partition_index == self.partition_index) and (
+                    self.accelerator == accelerator):
                 pass
             else:
                 self.network = network
                 self.partition_index = partition_index
-                self.head = tf.lite.Interpreter(
-                    model_path="../" + PATH_PREFIX[network] + "/models/head/" + str(partition_index) + ".tflite")
+                self.accelerator = accelerator
+                if self.accelerator:
+                    self.head = tflite.Interpreter(
+                        model_path="../" + PATH_PREFIX[network] + "/models/head/" + str(
+                            partition_index) + "_edgetpu.tflite",
+                        experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
+                else:
+                    self.head = tf.lite.Interpreter(
+                        model_path="../" + PATH_PREFIX[network] + "/models/head/" + str(partition_index) + ".tflite")
                 self.head.allocate_tensors()
 
                 # Get input and output tensors from head network.
@@ -124,30 +137,48 @@ class SplitComputeClient:
             network_time = end_network_time - start_network_time
             network_time = network_time - server_time
         client_time = end_counter_ns - start_counter_ns
-        return classes, client_time, server_time, network_time
+        total_time = client_time + server_time + network_time
+        return classes, client_time / 1000000, server_time / 1000000, network_time / 1000000, total_time / 1000000
 
 
-def run():
+def run(num_images, network_arg, accelerator):
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel("localhost:50051", [
+    with grpc.insecure_channel("192.168.167.81:50051", [
         ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),
         ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH),
     ]) as channel:
 
         stub = service_pb2_grpc.SplitServiceStub(channel)
         client = SplitComputeClient()
-        network = service_pb2.VGG16
-        num_images = 10
+        if network_arg == 'vgg16':
+            network = service_pb2.VGG16
+        elif network_arg == 'resnet50':
+            network = service_pb2.RESNET50
+        elif network_arg == 'mobilenetv2':
+            network = service_pb2.MOBILENETV2
+
+        data_dir = '/home/pi/tensorflow_datasets/downloads'
+        write_dir = '/home/pi/tensorflow_datasets/extracted'
+        download_config = tfds.download.DownloadConfig(
+            extract_dir=os.path.join(write_dir, 'extracted'),
+            manual_dir=data_dir
+        )
+        download_and_prepare_kwargs = {
+            'download_dir': os.path.join(write_dir, 'downloaded'),
+            'download_config': download_config,
+        }
 
         validation_ds, metadata = tfds.load(
             'imagenet2012',
             split='validation',
             with_info=True,
             as_supervised=True,
-        )
-        # TODO maybe better sampling?
+            data_dir=os.path.join(write_dir, 'data'),
+            download=True,
+            download_and_prepare_kwargs=download_and_prepare_kwargs)
+
         get_label_name = metadata.features['label'].int2str
 
         # preprocess according to network; and skip images used for quantization
@@ -158,18 +189,33 @@ def run():
         elif network == service_pb2.MOBILENETV2:
             validation_ds = validation_ds.map(normalize_img_mobilenetv2, num_parallel_calls=tf.data.AUTOTUNE).skip(100)
         accuracies = pd.DataFrame(range(LOCAL_COMPUTE_IDX[network] + 1), columns=['layer'])
+        latencies = pd.DataFrame()
         top1 = list()
         top5 = list()
+        layer = list()
+        img = list()
+        client_timings = list()
+        server_timings = list()
+        network_timings = list()
+        total_timings = list()
+
         for partition_index in tqdm(range(LOCAL_COMPUTE_IDX[network] + 1)):
             it = iter(validation_ds)
             top1_cnt = 0
             top5_cnt = 0
 
-            for _ in range(num_images):
+            for img_num in range(num_images):
                 image, label = next(it)
-                pred_classes, client_time, server_time, network_time = client.split_compute(stub, image,
-                                                                                            service_pb2.VGG16,
-                                                                                            partition_index)
+                pred_classes, client_time, server_time, network_time, total_time = client.split_compute(stub, image,
+                                                                                                        network,
+                                                                                                        partition_index,
+                                                                                                        accelerator)
+                layer.append(partition_index)
+                img.append(img_num)
+                client_timings.append(client_time)
+                server_timings.append(server_time)
+                network_timings.append(network_time)
+                total_timings.append(total_time)
                 if get_label_name(label) in pred_classes:
                     top5_cnt += 1
                 if get_label_name(label) == pred_classes[0]:
@@ -178,9 +224,32 @@ def run():
             top5.append(top5_cnt / num_images)
         accuracies['top1'] = top1
         accuracies['top5'] = top5
-        accuracies.to_csv("VGG16_CPU_accuracies.csv", index=False)
-        # TODO write timings into file!
+        latencies['layer'] = layer
+        latencies['img'] = img
+        latencies['client'] = client_timings
+        latencies['server'] = server_timings
+        latencies['network'] = network_timings
+        latencies['total'] = total_timings
+
+        if accelerator:
+            pu = "tpu"
+        else:
+            pu = "cpu"
+        accuracies.to_csv("../" + network_arg + "_" + pu + "_accuracies.csv", index=False)
+        latencies.to_csv("../" + network_arg + "_" + pu + "_latencies.csv", index=False)
+
+
+def read_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-a', '--accelerator', action=argparse.BooleanOptionalAction, help='Use TPU instead of CPU.',
+                        default=False)
+    parser.add_argument('-n', '--network', type=str, choices=['vgg16', 'resnet50', 'mobilenetv2'], default='vgg16',
+                        help='The network to be used.')
+    parser.add_argument('-s', '--n_samples', type=int, default=100, help='The number of samples to average over.')
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    run()
+    args = read_args()
+    run(args.n_samples, args.network, args.accelerator)
